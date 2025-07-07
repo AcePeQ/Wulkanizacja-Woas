e7add3be4de13ae23fca393e7e66c1cab75dfec1
AcePeQ <maciej.nojszewski22@gmail.com>
2025-07-07 22:32:24 +0200

    feat|style: add Animated heading, remove h1 visually but avaiable for SR. Simple mark up for Section hero

--- woas/src/components/HeroSection/HeroSection.tsx
+++ woas/src/components/HeroSection/HeroSection.tsx
@@ -1,9 +1,19 @@
+import styles from "./HeroSection.module.css";
+
+import AnimatedHeading from "../AnimatedHeader/AnimatedHeading";
 import Section from "../Section/Section";
 
 function HeroSection() {
   return (
     <Section sectionClass="hero" overlay="dark">
-      <div>Section</div>
+      <div className={styles.container}>
+        <AnimatedHeading />
+        <h1 className="sr_only">Wulkanizacja WOAS</h1>
+        <h2 className={styles.subHeading}>
+          Najlepsza wulkanizacja w Kałuszynie <br />
+          Przyjedz do nas lub zadzwoń a to my przybędziemy z pomocą
+        </h2>
+      </div>
     </Section>
   );
 }
