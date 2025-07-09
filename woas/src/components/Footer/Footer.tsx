b0a34a6b427ac348bdd793064c7afad844b6f59e
AcePeQ <maciej.nojszewski22@gmail.com>
2025-07-09 18:42:49 +0200

    feat: add footer

---
+++ woas/src/components/Footer/Footer.tsx
@@ -0,0 +1,24 @@
+import Logo from "../Logo/Logo";
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.left}>Telefon</div>
+      <div className={styles.middle}>
+        <Logo isLogoFooter />
+      </div>
+      <div className={styles.right}>
+        <p>&copy; Copyright WOAS 2025</p>
+        <p>
+          Made by:{" "}
+          <a className={styles.madeByLink} href="#">
+            Maciej Nojszewski
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- woas/src/components/Logo/Logo.tsx
+++ woas/src/components/Logo/Logo.tsx
@@ -1,6 +1,18 @@
 import styles from "./Logo.module.css";
 
-function Logo() {
+function Logo({ isLogoFooter = false }: { isLogoFooter?: boolean }) {
+  if (isLogoFooter) {
+    return (
+      <a
+        className={styles.logoFooter}
+        href="https://www.google.pl/maps/place/Mobilna+Wulkanizacja+Serwis+i+sprzeda%C5%BC+opon/@52.1966925,21.7578525,19z/data=!4m6!3m5!1s0x471f30c2b4edbb1b:0x684186c5a394fced!8m2!3d52.1966735!4d21.758097!16s%2Fg%2F11g697vpzw?hl=pl&entry=ttu&g_ep=EgoyMDI1MDcwNi4wIKXMDSoASAFQAw%3D%3D"
+        target="_blank"
+      >
+        WOAS
+      </a>
+    );
+  }
+
   return (
     <a className={styles.logo} href="#">
       🔧WOAS
