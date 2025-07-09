a85eb920114e52810763053f4e959d7620ae90e4
AcePeQ <maciej.nojszewski22@gmail.com>
2025-07-09 18:12:16 +0200

    style: section contact background image

---
+++ woas/src/components/Map/Map.tsx
@@ -0,0 +1,15 @@
+function Map() {
+  return (
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1222.7867847591!2d21.75754085372298!3d52.196623538071144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471f30c2b4edbb1b%3A0x684186c5a394fced!2sMobilna%20Wulkanizacja%20Serwis%20i%20sprzeda%C5%BC%20opon!5e0!3m2!1spl!2spl!4v1752075860277!5m2!1spl!2spl"
+      width="100%"
+      height="100%"
+      style={{ border: 0 }}
+      allowFullScreen={false}
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+    ></iframe>
+  );
+}
+
+export default Map;
--- woas/src/components/Sections/ContactSection/ContactSection.tsx
+++ woas/src/components/Sections/ContactSection/ContactSection.tsx
@@ -1,5 +1,6 @@
 import AnimatedLine from "../../AnimatedLine/AnimatedLine";
 import ContactInformations from "../../ContactInformations/ContactInformations";
+import Map from "../../Map/Map";
 import OpenHoursList from "../../OpenHoursList/OpenHoursList";
 import Section from "../../Section/Section";
 import styles from "./ContactSection.module.css";
@@ -25,15 +26,7 @@ function ContactSection() {
           </div>
         </article>
         <div className={styles.mapWrapper}>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1222.7867847591!2d21.75754085372298!3d52.196623538071144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471f30c2b4edbb1b%3A0x684186c5a394fced!2sMobilna%20Wulkanizacja%20Serwis%20i%20sprzeda%C5%BC%20opon!5e0!3m2!1spl!2spl!4v1752075860277!5m2!1spl!2spl"
-            width="100%"
-            height="100%"
-            style={{ border: 0 }}
-            allowFullScreen={false}
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+          <Map />
         </div>
       </div>
     </Section>
